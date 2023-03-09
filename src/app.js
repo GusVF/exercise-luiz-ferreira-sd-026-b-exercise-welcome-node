1d2872163bd7066b8ca342efba1a4bcb7eff8fe8
Luiz Gustavo Ferreira <gus.116506@gmail.com>
2023-03-09 20:00:28 -0300

    requisito-04

--- src/app.js
+++ src/app.js
@@ -6,13 +6,17 @@ app.use(express.json());
 
 app.get('/movies/:id', async (req, res) => {
   const { id } = req.params;
-  console.log(req.params);
 const fileContent = await readJsonData('./src/movies.json');
 const moviesList = fileContent.find((movie) => movie.id === Number(id));
    if (!moviesList) {
     return res.status(404).json({ message: 'Filme não encontrado' });
    }
     return res.status(200).json(moviesList);
+});
+
+app.get('/movies', async (req, res) => {
+    const fileContent = await readJsonData('./src/movies.json');
+    return res.status(200).json(fileContent);
 });
 
 module.exports = app;
